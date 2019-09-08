d9f8f8fc09c7df67b0fb15effcf0fc33a00d1ea8
izielin <misiopar@wp.pl>
2019-09-08 20:55:29 +0200

    requirements

--- main/admin.py
+++ main/admin.py
@@ -1,14 +1,5 @@
 from django.contrib import admin
-from django.db import models
+from .models import Post
 
-from . import models as demo_models
-from mdeditor.widgets import MDEditorWidget
-
-
-class ExampleModelAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        models.TextField: {'widget': MDEditorWidget}
-    }
-
-admin.site.register(demo_models.Post, ExampleModelAdmin)
+admin.site.register(Post)
 
